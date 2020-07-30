fde6f0b6c09c8b8d417f890473245d4077b43a17
asawamura <adam.sawamura@gmail.com>
2020-07-30 08:55:35 -0400

    second

--- app.py
+++ app.py
@@ -1,8 +1,9 @@
 # ---- YOUR APP STARTS HERE ----
 # -- Import section --
 from flask import Flask
-# from flask import render_template
-# from flask import request
+from flask import render_template
+from flask import request
+from model import *
 
 
 # -- Initialization section --
@@ -13,4 +14,12 @@ app = Flask(__name__)
 @app.route('/')
 @app.route('/index')
 def index():
-    return "hello world"
+    return render_template("index.html")
+
+@app.route('/results', methods=['GET', 'POST'])
+def results():
+    user_breakfast = request.form["breakfast"]
+    user_name = request.form["nickname"]
+    breakfast_rating = get_breakfast_rating(user_breakfast)
+    return render_template("results.html", user_breakfast=user_breakfast, user_name=user_name, breakfast_rating=breakfast_rating)
+
---
+++ model.py
@@ -0,0 +1,5 @@
+def get_breakfast_rating(food):
+    if food.lower() == "waffles":
+        return "That's an amazing breakfast!"
+    else:
+        return "That's a good breakfast."
